3dc81c54cef109b1529ff52ccc2c5fedb2ce92e0
Firdaus <mohdfirdaus94@outlook.com>
2021-12-14 09:21:18 +0800

    initial commit

---
+++ assesment1/src/main/java/com/ibftfip2021/App.java
@@ -0,0 +1,13 @@
+package com.ibftfip2021;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
